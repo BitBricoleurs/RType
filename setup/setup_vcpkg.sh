bc8c645afb78849945a5107c181faada283811c6
Alexandre <alexandre.lagasse@epitech.eu>
2023-09-20 17:37:11 +0200

    build (project): add sh script for vcpkg

--- setup/setup_vcpkg.sh
+++ setup/setup_vcpkg.sh
@@ -1,17 +1,8 @@
-#!/bin/bash
-
 VCPKG_DIR="./vcpkg"
 
 LIBRARIES=(
-  "sdl-net"
+  "sdl2-net"
 )
-
-if [ ! -d "$VCPKG_DIR" ]; then
-  git clone https://github.com/microsoft/vcpkg.git $VCPKG_DIR
-  pushd $VCPKG_DIR
-  ./bootstrap-vcpkg.sh
-  popd
-fi
 
 for LIB in "${LIBRARIES[@]}"; do
   $VCPKG_DIR/vcpkg install $LIB
