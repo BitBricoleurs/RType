43ff7780e9a337bd88f06dfba519e4e58d7b4e23
Alexandre <alexandre.lagasse@epitech.eu>
2023-09-25 15:10:59 +0200

    build (setup scripts): add relative path to scripts

--- setup/setup_vcpkg.bat
+++ setup/setup_vcpkg.bat
@@ -3,7 +3,7 @@ set LIBRARIES=sdl2-net
 
 for %%L in (%LIBRARIES%) do (
     echo Telechargement et installation de la bibliotheque: %%L
-    vcpkg install %%L
+    ./vcpkg/vcpkg install %%L
 )
 
 echo Fin de l'installation des bibliotheques.
