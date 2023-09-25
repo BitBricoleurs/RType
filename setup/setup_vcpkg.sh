6258b9304c6667b4ca82738871eb447e2a516570
Alexandre <alexandre.lagasse@epitech.eu>
2023-09-25 13:58:53 +0200

    fix (build): delete useless files

--- setup/setup_vcpkg.sh
+++ setup/setup_vcpkg.sh
@@ -1,11 +1,9 @@
-VCPKG_DIR="./vcpkg"
-
 LIBRARIES=(
   "sdl2-net"
 )
 
 for LIB in "${LIBRARIES[@]}"; do
-  $VCPKG_DIR/vcpkg install $LIB
+  vcpkg install $LIB
 done
 
 echo "Installation terminée !"
