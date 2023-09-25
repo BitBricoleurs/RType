cbdfe4a06723a443c6b10ad01afe2dce26689939
harryviennot <harry.viennot@epitech.eu>
2023-09-25 15:55:28 +0200

    ci: added linter

--- format.sh
+++ format.sh
@@ -6,3 +6,8 @@
 find . -iname *.cpp -o -iname *.h -o -iname *.hpp -o -iname *.cxx | xargs clang-format -i
 
 echo "Format completed."
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .
+find . -name '*.cpp' -or -name '*.h' | xargs clang-tidy -p .
+
+echo "Tidy completed."
