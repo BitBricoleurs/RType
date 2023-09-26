93d2b6dffa18be0c29f759d0f05cc38e2e8fc53c
harryviennot <harry.viennot@epitech.eu>
2023-09-26 20:24:07 +0200

    fix(game): removed static factory functions

--- src/RType/Entities/EntityFactory.hpp
+++ src/RType/Entities/EntityFactory.hpp
@@ -18,28 +18,28 @@ class EntityFactory {
   public:
     EntityFactory() = default;
     ~EntityFactory() = default;
-    static size_t createBaseMob(GameEngine& engine, 
+    size_t createBaseMob(GameEngine& engine, 
         const std::string& spriteSheetPath, int rectX, int rectY, int rectWidth, int rectHeight, 
         float posX, float posY, float velX, float velY, float dirX, float dirY, 
         float hitboxWidth, float hitboxHeight, int maxHealth, int damageValue, float bulletStartX, float bulletStartY);
 
-    static size_t createBossMob(GameEngine& engine, 
+    size_t createBossMob(GameEngine& engine, 
         const std::string& spriteSheetPath, int rectX, int rectY, int rectWidth, int rectHeight, 
         float posX, float posY, float velX, float velY, float dirX, float dirY, 
         float hitboxWidth, float hitboxHeight, int maxHealth, int damageValue, float bulletStartX, float bulletStartY,
         int stageValue);
 
-    static size_t createPlayer(GameEngine& engine, 
+    size_t createPlayer(GameEngine& engine, 
         const std::string& spriteSheetPath, int rectX, int rectY, int rectWidth, int rectHeight, 
         float posX, float posY, float velX, float velY, float dirX, float dirY, 
         float hitboxWidth, float hitboxHeight, int maxHealth, float bulletStartX, float bulletStartY);
 
-    static size_t createBullet(GameEngine& engine, 
+    size_t createBullet(GameEngine& engine, 
         const std::string& spriteSheetPath, int rectX, int rectY, int rectWidth, int rectHeight, 
         float posX, float posY, float velX, float velY, float dirX, float dirY, 
         float hitboxWidth, float hitboxHeight, int damageValue);
 
-    static size_t createPowerUp(GameEngine& engine, 
+    size_t createPowerUp(GameEngine& engine, 
         const std::string& spriteSheetPath, int rectX, int rectY, int rectWidth, int rectHeight, 
         float posX, float posY, float velX, float velY, float dirX, float dirY, 
         float hitboxWidth, float hitboxHeight);
@@ -47,7 +47,7 @@ class EntityFactory {
   protected:
 
   private:
-    static size_t createBaseEntity(GameEngine& engine,const std::string& spriteSheetPath, int rectX, int rectY, int rectWidth,
+    size_t createBaseEntity(GameEngine& engine,const std::string& spriteSheetPath, int rectX, int rectY, int rectWidth,
         int rectHeight,  float posX, float posY, float velX, float velY, float dirX, float dirY, float hitboxWidth, float hitboxHeight);
 };
 
