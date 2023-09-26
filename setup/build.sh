bdb657ac919e31c53b31ed1fef02e98b7272b58f
Alexandre Lagasse <alexandre.lagasse@epitech.eu>
2023-09-26 16:09:50 +0200

    ci (setup): add libraries dependencies

--- setup/build.sh
+++ setup/build.sh
@@ -6,7 +6,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
         echo "Homebrew n'est pas installé. Veuillez l'installer d'abord."
         exit 1
     fi
-    brew install mesa libx11 libxcursor libxinerama libxrandr
+    brew install mesa libx11 libxcursor libxinerama libxrandr xinerama xcursor xorg libglu pkg-config
 else
     if command -v apt >/dev/null 2>&1 ; then
         PACKAGE_MANAGER="apt"
@@ -21,11 +21,11 @@ else
 
     if [ "$PACKAGE_MANAGER" == "apt" ]; then
         sudo apt update
-        sudo apt install -y libgl1-mesa-dev libx11-dev libxcursor-dev libxinerama-dev libxrandr-dev libasound2-dev
+        sudo apt install -y libgl1-mesa-dev libx11-dev libxcursor-dev libxinerama-dev libxrandr-dev libasound2-dev xinerama xcursor xorg libglu1-mesa-dev pkg-config
     elif [ "$PACKAGE_MANAGER" == "dnf" ]; then
-        sudo dnf install -y mesa-libGL-devel libX11-devel libXcursor-devel libXinerama-devel libXrandr-devel alsa-lib-devel
+        sudo dnf install -y mesa-libGL-devel libX11-devel libXcursor-devel libXinerama-devel libXrandr-devel alsa-lib-devel xinerama xcursor xorg-x11-server libGLU pkg-config
     elif [ "$PACKAGE_MANAGER" == "pacman" ]; then
-        sudo pacman -Syu --needed mesa libx11 libxcursor libxinerama libxrandr alsa-lib
+        sudo pacman -Syu --needed mesa libx11 libxcursor libxinerama libxrandr alsa-lib xorg-xineramaproto xorg-xcursor xorg-server libglu pkg-config
     fi
 fi
 
