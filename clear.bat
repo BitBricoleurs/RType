70779e0de72375669b9bee74aaf015dd2c21d003
Alexandre lgs <alexandre.lagasse@epitech.eu>
2023-09-27 13:04:12 +0200

    ci (scripts): fix add add windows script

--- clear.bat
+++ clear.bat
@@ -2,6 +2,9 @@
 echo Nettoyage des fichiers et dossiers générés...
 
 rd /s /q build
+del /f /q conan-win-64.zip
 del /f /q POCProgram
+del /f /q POCProgram.exe
+del /f /q POCProgram.pdb
 
 echo Nettoyage terminé!
--- setup/clone_conan.ps1
+++ setup/clone_conan.ps1
@@ -1,22 +1,24 @@
-# Script.ps1
+# Crée le dossier "build" s'il n'existe pas
+if (-not (Test-Path .\build)) {
+    New-Item -Path .\build -ItemType Directory
+}
 
-# Vérifier si Python est installé
-if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
-    Write-Output "Python is required but not installed. Please install Python."
+# Télécharge l'archive Conan depuis le dernier release sur GitHub
+Invoke-WebRequest -Uri "https://github.com/conan-io/conan/releases/latest/download/conan-win-64.zip" -OutFile conan-win-64.zip
+
+# Vérifie si l'archive a été correctement téléchargée
+if (-not (Test-Path conan-win-64.zip)) {
+    Write-Error "Erreur lors du téléchargement de l'archive Conan."
     exit 1
 }
 
-# Vérifier si pip est installé
-if (-not (Get-Command pip -ErrorAction SilentlyContinue)) {
-    Write-Output "pip is required but not installed. Please install pip."
+# Extrait l'archive dans le dossier "build"
+Expand-Archive .\conan-win-64.zip -DestinationPath .\build
+
+# Vérifie si Conan a été correctement extrait
+if (-not (Test-Path .\build\conan.exe)) {
+    Write-Error "Erreur lors de l'extraction de Conan dans le dossier build."
     exit 1
 }
 
-# Installer Conan via pip
-pip install conan
-if ($LASTEXITCODE -ne 0) {
-    Write-Output "Failed to install Conan using pip."
-    exit 1
-}
-
-exit 0
+Write-Output "Conan a été correctement téléchargé et extrait dans le dossier build."
