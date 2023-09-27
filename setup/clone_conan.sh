c943ebe81384363701b7472817ff964984f72ba2
Alexandre Lagasse <alexandre.lagasse@epitech.eu>
2023-09-27 10:47:09 +0200

    ci (script): fix clone script

--- setup/clone_conan.sh
+++ setup/clone_conan.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-if ! command -v python &> /dev/null; then
-    echo "Python is required but not installed. Please install Python."
-    exit 1
-fi
-
 if ! command -v pip &> /dev/null; then
     echo "pip is required but not installed. Please install pip."
     exit 1
 fi
 
 # Install Conan
+
 pip install conan
 if [ $? -ne 0 ]; then
     echo "Failed to install Conan using pip."
