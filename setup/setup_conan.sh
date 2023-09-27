f601ce23e411a41c9376a2dd384ab3c28b172ef8
Alexandre Lagasse <alexandre.lagasse@epitech.eu>
2023-09-27 11:24:23 +0200

    ci (script): fix auto install in insall command

--- setup/setup_conan.sh
+++ setup/setup_conan.sh
@@ -3,7 +3,6 @@
 # Mettre à jour le profil par défaut avec la détection automatique
 conan profile detect --force
 
-conan profile update general.tools.system.package_manager:mode=install default
 # Vérifiez si la commande précédente a réussi
 if [ $? -ne 0 ]; then
     echo "Erreur lors de la détection du profil Conan."
@@ -12,7 +11,7 @@ fi
 
 # Installer les dépendances spécifiées dans conanfile.txt
 
-conan install setup/conanfile.txt --output-folder=build --build=missing
+conan install setup/conanfile.txt --output-folder=build --build=missing -c tools.system.package_manager:mode=install
 
 # Vérifiez si la commande précédente a réussi
 if [ $? -ne 0 ]; then
