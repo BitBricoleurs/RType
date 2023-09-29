6b6c050c9034cd02ab283eef29f18056ef26142c
harryviennot <harry.viennot@epitech.eu>
2023-09-29 13:15:33 +0200

    feat(game): made factory singleton

--- src/RType/Entities/EntityFactory.hpp
+++ src/RType/Entities/EntityFactory.hpp
@@ -16,17 +16,24 @@
 
 class EntityFactory {
   public:
-    EntityFactory() = default;
-    ~EntityFactory() = default;
-    size_t createBaseMob(GameEngine::GameEngine& engine, const std::string& spriteSheetPath, int rectX, int rectY,
-                         int rectWidth, int rectHeight, float posX, float posY, float velX, float velY, float dirX,
-                         float dirY, float hitboxWidth, float hitboxHeight, int maxHealth, int damageValue,
-                         float bulletStartX, float bulletStartY);
+    static EntityFactory& getInstance() {
+        static EntityFactory instance;
+        return instance;
+    }
+    EntityFactory(const EntityFactory&) = delete;
+    EntityFactory& operator=(const EntityFactory&) = delete;
 
-    size_t createBossMob(GameEngine::GameEngine& engine, const std::string& spriteSheetPath, int rectX, int rectY,
-                         int rectWidth, int rectHeight, float posX, float posY, float velX, float velY, float dirX,
-                         float dirY, float hitboxWidth, float hitboxHeight, int maxHealth, int damageValue,
-                         float bulletStartX, float bulletStartY, int stageValue);
+    size_t createBaseMob(GameEngine::GameEngine& engine, const std::string& spriteSheetPath, int spriteSheetHeight,
+                         int spriteSheetWidth, int frames, bool twoDirections, const std::string& deathSpriteSheetPath,
+                         int deathSpriteSheetHeight, int deathSpriteSheetWidth, int deathFrames, float posX, float posY,
+                         float velX, float velY, float dirX, float dirY, float hitboxWidth, float hitboxHeight,
+                         int maxHealth, int damageValue, float bulletStartX, float bulletStartY);
+
+    size_t createBossMob(GameEngine::GameEngine& engine, const std::string& spriteSheetPath, int spriteSheetHeight,
+                         int spriteSheetWidth, int frames, bool twoDirections, const std::string& deathSpriteSheetPath,
+                         int deathSpriteSheetHeight, int deathSpriteSheetWidth, int deathFrames, float posX, float posY,
+                         float velX, float velY, float dirX, float dirY, float hitboxWidth, float hitboxHeight,
+                         int maxHealth, int damageValue, float bulletStartX, float bulletStartY, int stageValue);
 
     size_t createPlayer(GameEngine::GameEngine& engine, const std::string& spriteSheetPath, int rectX, int rectY,
                         int rectWidth, int rectHeight, float posX, float posY, float velX, float velY, float dirX,
@@ -42,12 +49,17 @@ class EntityFactory {
                          float dirY, float hitboxWidth, float hitboxHeight);
 
   private:
+    EntityFactory() = default;
+    ~EntityFactory() = default;
     size_t createBaseEntity(GameEngine::GameEngine& engine, const std::string& spriteSheetPath, int spriteSheetHeight,
                             int spriteSheetWidth, int frames, bool twoDirections, float posX, float posY, float velX,
                             float velY, float dirX, float dirY, float hitboxWidth, float hitboxHeight);
 
     std::shared_ptr<GameEngine::SpriteAnimationComponent> initAnimation(const std::string& spriteSheetPath, int frames,
                                                                         int width, int height, bool twoDirections);
+
+    std::shared_ptr<GameEngine::DeathAnimationComponent>
+    initDeathAnimation(const std::string& deathSpriteSheetPath, int deathFrames, int deathWidth, int deathHeight);
 };
 
 #endif /* !ENTITYFACTORY_HPP_ */
