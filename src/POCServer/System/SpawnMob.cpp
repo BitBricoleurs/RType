7999a76b6a1fcfc346a43fbc886a4e729d71d53a
Alexandre Lagasse <alexandre.lagasse@epitech.eu>
2023-10-16 12:03:01 +0200

    fix(macos-windows): fix if os

--- src/POCServer/System/SpawnMob.cpp
+++ src/POCServer/System/SpawnMob.cpp
@@ -7,7 +7,9 @@
 
 #include "SpawnMob.hpp"
 #include "SpawnMob.hpp"
-#if defined(__linux__)
+#if defined(__APPLE__)
+#include <mach-o/dyld.h>
+#elif defined(__linux__)
 #include <libgen.h>
 #include <limits.h>
 #include <unistd.h>
@@ -57,8 +59,10 @@ void SpawnMob::update(GameEngine::ComponentsContainer &componentsContainer, Game
 
 void SpawnMob::loadMapFiles(const std::string &path)
 {
-    std::string newPath = std::string();
-    #if defined(__APPLE__)
+    std::string newPath = std::string("");
+    #if defined(_WIN32) || defined(_WIN64)
+        newPath = "";
+    #elif defined(__APPLE__)
         char pathd[1024];
         uint32_t size = sizeof(pathd);
         if (_NSGetExecutablePath(pathd, &size) == 0) {
@@ -69,7 +73,7 @@ void SpawnMob::loadMapFiles(const std::string &path)
     }
     #else
         char result[PATH_MAX];
-        ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+        size_t count = readlink("/proc/self/exe", result, PATH_MAX);
         if (count < 0 || count >= PATH_MAX) {
             newPath = "";
         }
