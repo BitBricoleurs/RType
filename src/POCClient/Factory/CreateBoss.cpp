4d2464dee751b75f9914e6c8741d13537267807a
harryviennot <harry.viennot@epitech.eu>
2023-10-23 18:34:43 +0200

    update

--- src/POCClient/Factory/CreateBoss.cpp
+++ src/POCClient/Factory/CreateBoss.cpp
@@ -10,31 +10,34 @@
 #include <cmath>
 #include <cstddef>
 
+namespace Client {
 size_t
 EntityFactory::createBellmiteBoss(GameEngine::ComponentsContainer &container,
                                   GameEngine::EventHandler &eventHandler,
-                                  GameEngine::Vect2 pos,
-                                  GameEngine::Vect2 velocity) {
-  nlohmann::json config = loadConfig("config/Entity/createBossBellmite.json");
-  const auto &bossConfig = config["createBellmiteBoss"];
+                                  Utils::Vect2 pos, Utils::Vect2 velocity) {
+  LoadConfig::ConfigData data =
+      LoadConfig::LoadConfig::getInstance().loadConfig(
+          "config/Entity/createBellmite.json");
+  td::string bossKey = "/createBellmiteBoss";
+  std::string path = data.getString(bossKey + "/spriteSheetPath");
 
   size_t entityId = createBossMob(
-      container, bossConfig["spriteSheetPath"].get<std::string>(),
-      bossConfig["spriteSheetHeight"].get<int>(),
-      bossConfig["spriteSheetWidth"].get<int>(),
-      bossConfig["frames"].get<int>(), bossConfig["twoDirections"].get<bool>(),
-      bossConfig["reverse"].get<bool>(),
-      bossConfig["deathSpriteSheetPath"].get<std::string>(),
-      bossConfig["deathSpriteSheetHeight"].get<int>(),
-      bossConfig["deathSpriteSheetWidth"].get<int>(),
-      bossConfig["deathFrames"].get<int>(), pos, velocity,
-      bossConfig["playerA"].get<int>(), bossConfig["scale"].get<float>(),
-      bossConfig["rotation"].get<float>(),
-      GameEngine::ColorR(bossConfig["tint"]["r"].get<int>(),
-                         bossConfig["tint"]["g"].get<int>(),
-                         bossConfig["tint"]["b"].get<int>(),
-                         bossConfig["tint"]["a"].get<int>()),
-      bossConfig["layer"].get<int>());
+      container, path, data.getInt(bossKey + "/spriteSheetHeight"),
+      data.getInt(bossKey + "/spriteSheetWidth"),
+      data.getInt(bossKey + "/frames"),
+      data.getBool(bossKey + "/twoDirections"),
+      data.getBool(bossKey + "/reverse"),
+      data.getString(bossKey + "/deathSpriteSheetPath"),
+      data.getInt(bossKey + "/deathSpriteSheetHeight"),
+      data.getInt(bossKey + "/deathSpriteSheetWidth"),
+      data.getInt(bossKey + "/deathFrames"), pos, velocity,
+      data.getInt(bossKey + "/playerA"), data.getFloat(bossKey + "/scale"),
+      data.getFloat(bossKey + "/rotation"),
+      Utils::ColorR(
+          data.getInt(bossKey + "/tint/r"), data.getInt(bossKey + "/tint/g"),
+          data.getInt(bossKey + "/tint/b"), data.getInt(bossKey + "/tint/a")),
+      data.getInt(bossKey + "/layer"));
+
   auto bossCore = std::make_shared<isBossCore>();
   container.bindComponentToEntity(entityId, bossCore);
   container.unbindComponentFromEntity(
@@ -47,27 +50,33 @@ EntityFactory::createBellmitePod(GameEngine::ComponentsContainer &container,
                                  GameEngine::EventHandler &eventHandler,
                                  GameEngine::Vect2 pos,
                                  GameEngine::Vect2 velocity) {
-  nlohmann::json config = loadConfig("config/Entity/createBossBellmite.json");
-  const auto &podConfig = config["createBellmitePod"];
+  LoadConfig::ConfigData data =
+      LoadConfig::LoadConfig::getInstance().loadConfig(
+          "config/Entity/createBellmite.json");
+  td::string podKey = "/createBellmitePod";
+  std::string path = data.getString(podKey + "/spriteSheetPath");
 
   size_t entityId = createBossMob(
-      container, podConfig["spriteSheetPath"].get<std::string>(),
-      podConfig["spriteSheetHeight"].get<int>(),
-      podConfig["spriteSheetWidth"].get<int>(), podConfig["frames"].get<int>(),
-      podConfig["twoDirections"].get<bool>(), podConfig["reverse"].get<bool>(),
-      podConfig["deathSpriteSheetPath"].get<std::string>(),
-      podConfig["deathSpriteSheetHeight"].get<int>(),
-      podConfig["deathSpriteSheetWidth"].get<int>(),
-      podConfig["deathFrames"].get<int>(), pos, velocity,
-      podConfig["playerA"].get<int>(), podConfig["scale"].get<float>(),
-      podConfig["rotation"].get<float>(),
-      GameEngine::ColorR(
-          podConfig["tint"]["r"].get<int>(), podConfig["tint"]["g"].get<int>(),
-          podConfig["tint"]["b"].get<int>(), podConfig["tint"]["a"].get<int>()),
-      podConfig["layer"].get<int>());
+      container, path, data.getInt(podKey + "/spriteSheetHeight"),
+      data.getInt(podKey + "/spriteSheetWidth"),
+      data.getInt(podKey + "/frames"), data.getBool(podKey + "/twoDirections"),
+      data.getBool(podKey + "/reverse"),
+      data.getString(podKey + "/deathSpriteSheetPath"),
+      data.getInt(podKey + "/deathSpriteSheetHeight"),
+      data.getInt(podKey + "/deathSpriteSheetWidth"),
+      data.getInt(podKey + "/deathFrames"), pos, velocity,
+      data.getInt(podKey + "/playerA"), data.getFloat(podKey + "/scale"),
+      data.getFloat(podKey + "/rotation"),
+      Utils::ColorR(
+          data.getInt(podKey + "/tint/r"), data.getInt(podKey + "/tint/g"),
+          data.getInt(podKey + "/tint/b"), data.getInt(podKey + "/tint/a")),
+      data.getInt(podKey + "/layer"));
+
   auto bossPod = std::make_shared<isBossPod>();
   container.bindComponentToEntity(entityId, bossPod);
   eventHandler.scheduleEvent("animate", 8,
                              std::make_tuple(std::string("Pods"), entityId));
   return entityId;
 }
+
+} // namespace Client
