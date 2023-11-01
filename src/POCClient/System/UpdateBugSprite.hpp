1c1644a90ec093182682b51d80aa9388e625008d
harryviennot <harry.viennot@epitech.eu>
2023-11-01 23:37:02 +0100

    add pi

--- src/POCClient/System/UpdateBugSprite.hpp
+++ src/POCClient/System/UpdateBugSprite.hpp
@@ -14,6 +14,10 @@
 #include "SpriteAnimation.hpp"
 #include "MobComponents.hpp"
 #include "SpriteComponent.hpp"
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+#include <cmath>
 
 namespace Client {
     class UpdateBugSprite : public GameEngine::ISystem {
